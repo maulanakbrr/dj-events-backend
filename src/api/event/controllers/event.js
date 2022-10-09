300d9363230e040566963b27ff46251393dccc8b
Akbar Maulana <amaulanaa995@gmail.com>
2022-10-09 22:21:10 +0700

    created new custom events/me - updated event controller and routes

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -6,4 +6,30 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::event.event');
+module.exports = createCoreController("api::event.event", ({ strapi }) => ({
+  // Get logged in users
+  async me(ctx) {
+    console.log('CTX: ', ctx.state)
+    const user = ctx.state.user;
+ 
+    if (!user) {
+      return ctx.badRequest(null, [
+        { message: "No authorization header was found" },
+      ]);
+    }
+ 
+    const data = await strapi.db.query("api::event.event").findMany({
+      where: {
+        user: { id: user.id },
+      },
+      populate: { user: true, image: true },
+    });
+    if (!data) {
+      return ctx.notFound();
+    }
+ 
+    const res = await this.sanitizeOutput(data, ctx);
+    return res;
+  },
+}));
+
