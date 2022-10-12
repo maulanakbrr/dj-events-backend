5b10b615355b4ff3727e00b6ccfabadd488173fc
Akbar Maulana <amaulanaa995@gmail.com>
2022-10-12 13:26:55 +0700

    updated database on production

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,10 +1,10 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
+module.exports = () => ({
   connection: {
-    client: 'sqlite',
+    client: "sqlite",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(__dirname, "..", ".tmp/data.db"),
     },
     useNullAsDefault: true,
   },
